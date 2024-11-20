563bbfb9f685c77c6da6f4f08e2f2d53f1d9e17b
SHAIKATHAZRA <shaikathazra@gmail.com>
2024-11-20 22:00:02 +0600

    save model

--- main.py
+++ main.py
@@ -51,3 +51,9 @@ model.add(layers.Dense(10,activation='softmax'))
 model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 
 model.fit(training_images, training_labels, epochs=10, validation_data=(testing_images,testing_labels))
+
+loss, accuracy =model.evaluate(testing_images,testing_labels)
+print(f"Loss:{loss}")
+print(f"Accuracy:{accuracy}")
+
+model.save("img_classifier.model")
