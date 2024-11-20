1384b302913d225f7bf370b80c804c86569a817f
SHAIKATHAZRA <shaikathazra@gmail.com>
2024-11-20 21:54:10 +0600

    haha traind

--- main.py
+++ main.py
@@ -38,11 +38,11 @@ testing_images=testing_images[:10000]
 testing_labels=testing_labels[:10000]
 
 model=models.Sequential()
-model.add(layers.conv2D(32,(3,3),activation='relu',input_shape=(32,32,3)))
+model.add(layers.Conv2D(32,(3,3),activation='relu',input_shape=(32,32,3)))
 model.add(layers.MaxPooling2D((2,2)))
-model.add(layers.conv2D(64,(3,3),activation='relu'))
+model.add(layers.Conv2D(64,(3,3),activation='relu'))
 model.add(layers.MaxPooling2D((2,2)))
-model.add(layers.conv2D(64,(3,3),activation='relu'))
+model.add(layers.Conv2D(64,(3,3),activation='relu'))
 model.add(layers.Flatten())
 model.add(layers.Dense(64,activation='relu'))
 model.add(layers.Dense(10,activation='softmax'))
